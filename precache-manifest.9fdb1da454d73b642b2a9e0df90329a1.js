c26e6ea5c3230d4b52858f02181ddd80e516968b
barrystone <barrystone3183@gmail.com>
2020-06-14 22:28:56 +0800

    Updates

---
+++ precache-manifest.9fdb1da454d73b642b2a9e0df90329a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66aca5ed83632fa52accfd356793a09d",
+    "url": "/Pingtung_Univ-quickLinks/index.html"
+  },
+  {
+    "revision": "7965fb63d7479d93dda5",
+    "url": "/Pingtung_Univ-quickLinks/static/css/main.a666396d.chunk.css"
+  },
+  {
+    "revision": "515b8c1a734d432cf282",
+    "url": "/Pingtung_Univ-quickLinks/static/js/2.32355a50.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Pingtung_Univ-quickLinks/static/js/2.32355a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7965fb63d7479d93dda5",
+    "url": "/Pingtung_Univ-quickLinks/static/js/main.27ea52d2.chunk.js"
+  },
+  {
+    "revision": "511f63177ac38c81e809",
+    "url": "/Pingtung_Univ-quickLinks/static/js/runtime-main.974bbd9d.js"
+  }
+]);
